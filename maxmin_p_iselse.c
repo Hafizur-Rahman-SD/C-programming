f737982684e5ef76451c87d8afd7fc9eb9ec26dc
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-11-23 15:38:54 +0600

    Add files via upload

---
+++ maxmin_p_iselse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>  //Heder file.
+
+int maximum (int,int,int);   //declear a variable type and make a function.
+int main(){                     // main function start point.
+	int x,y,z;                  //declear  variable name. 
+	printf("Enter thtee Integer value");  //we request to user to inter 3 integer type input. 
+	scanf("%d %d %d", &x,&y,&z);           // here console take and prosses our input.
+	printf("the maximum value is: %d", maximum(x,y,z));  // here prosses maximun and minimum value.
+}
+int maximum( int x, int y,int z){  //here we call the function and check by condition.
+	int max=x;
+	if(y>max)
+	max=y;
+	if(z>max)
+	max=z;
+	return max;
+}
