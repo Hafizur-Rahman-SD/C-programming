02e5ed0472e37608e13df7b94001d4609e02d478
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2024-01-15 10:40:28 +0600

    Update type_casting.c

--- type_casting.c
+++ type_casting.c
@@ -1,3 +1,6 @@
+// Implact type conversion in C.
+//implact time conversion as previously stoled, involues the automated conversion of one types value to another types value.
+
 #include<stdio.h>
 int main(){
   char alpha ='a';
